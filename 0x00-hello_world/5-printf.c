7a2656eb6dde6bfb36d31676a202bf39a64a3135
haliluc <haliluchristabel@gmail.com>
2023-08-12 06:54:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - A program that prints a line useing the printf function
  * Return: 0 (success)
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+return(0);
 }
