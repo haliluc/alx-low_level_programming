e31053f443fefd05fabc01f5644b4754bd7394d9
haliluc <haliluchristabel@gmail.com>
2023-08-13 12:40:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- *
- * Return: 1
- */
+* main - A c program that prints a line to the standard error
+* Return: 1
+*/
 int main(void)
 {	
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 70);
 return (1);
 }
