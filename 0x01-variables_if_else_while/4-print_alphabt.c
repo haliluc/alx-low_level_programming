c2ee5a85ad273b8ec6c62cf160ae155561ab7ae3
haliluc <haliluchristabel@gmail.com>
2023-08-14 05:41:38 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry  point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	      char c;
+
+	      c = 'a';
+	      while
+		      (c <= 'a') {
+			      if ((c t= 'q' 
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+The "0x01. C - Variables, if, else, while" with 13 task
