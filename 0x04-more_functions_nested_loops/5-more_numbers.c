32f7fccab2ff4aecdc4fe651f045f68abb73f276
haliluc <haliluchristabel@gmail.com>
2023-08-19 10:09:45 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more numbers - print from number 0 to 14 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int i, j;
+for (i = 1; i <= 10; i++)
+{
+	for (j = 0; j <= 14; j++)
+	{
+		if (j >= 10)
+			_putchar('1');
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+}
+
+}
