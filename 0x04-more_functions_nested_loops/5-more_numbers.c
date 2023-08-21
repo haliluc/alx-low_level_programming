b6d9e86628460f336eb68cc5f3ed16d3208bae12
haliluc <haliluchristabel@gmail.com>
2023-08-20 17:33:49 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more numbers - print from number 0 to 14 10 times
- * Return: void
+ * Return:void
  */
 
 void more_numbers(void)
@@ -12,8 +12,7 @@ for (i = 1; i <= 10; i++)
 {
 	for (j = 0; j <= 14; j++)
 	{
-		if (j >= 10)
-			_putchar('1');
+		_putchar('1');
 		_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
