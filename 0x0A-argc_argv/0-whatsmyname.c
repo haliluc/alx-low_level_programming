945d1900cee7f78068f6e6c0a419699daa53e4f7
haliluc <haliluchristabel@gmail.com>
2023-09-03 07:01:31 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: counts arguements
+ * @argv: arguement vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
